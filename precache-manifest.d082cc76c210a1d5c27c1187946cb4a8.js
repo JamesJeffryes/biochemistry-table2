e13c3e413c97bd67623667a5db2dfa8ccf0f9795
JamesJeffryes <jamesgjeffryes@gmail.com>
2019-03-19 11:27:32 -0500

    Updates

---
+++ precache-manifest.d082cc76c210a1d5c27c1187946cb4a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b12b1437eb69c320eb2",
+    "url": "/biochemistry-table2/static/js/runtime~main.a2fb84b7.js"
+  },
+  {
+    "revision": "39aae8ae8f418ef33b34",
+    "url": "/biochemistry-table2/static/js/main.65966e59.chunk.js"
+  },
+  {
+    "revision": "a3bf0df115e0dbf8f01b",
+    "url": "/biochemistry-table2/static/js/2.60e4c145.chunk.js"
+  },
+  {
+    "revision": "39aae8ae8f418ef33b34",
+    "url": "/biochemistry-table2/static/css/main.54d52055.chunk.css"
+  },
+  {
+    "revision": "a3bf0df115e0dbf8f01b",
+    "url": "/biochemistry-table2/static/css/2.f34d03c6.chunk.css"
+  },
+  {
+    "revision": "f8ac8159cacb0590d85d7c3c14e5e021",
+    "url": "/biochemistry-table2/index.html"
+  }
+];
